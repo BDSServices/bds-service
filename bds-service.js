be111e99863cb59c8349fb6c66d90308aa974c4b
BDSServices <bookdatasolutions@gmail.com>
2022-08-14 21:13:30 -0400

    bds-service updates

--- bds-service.js
+++ bds-service.js
@@ -1,17 +1,21 @@
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+//const qs = require('querystring');
 const bdsutil = require('./src/api/bds-util.js');
 
 
 const hostname = '127.0.0.1';
 const port = 8080;
 const server = http.createServer(async (req, res) => {
-    const {headers, method, url} = req;
-    console.log(method, url);
+    const {headers, method} = req;
+    const pathname = url.parse(req.url,true).pathname;
+    const qs = url.parse(req.url,true).query;
+    console.log(method, pathname);
     switch (method) {
         case 'GET':
-            switch(url) {
+            switch(pathname) {
                 case '/':
                     res.setHeader('content-Type', 'text/html');
                     res.writeHead(200);
@@ -21,17 +25,24 @@ const server = http.createServer(async (req, res) => {
                     });
                     break;
                 case '/home':
-                case '/search':
-                    const titles = await bdsutil.searchTitles('Madhu', 'file1.xml.json', 'title', 'Title 00000', 1);
-                    console.log(titles);
+                //case '/search':
+                    // const titles = await bdsutil.searchTitles('Madhu', 'file1.xml.json', 'title', 'Title 00000', 1);
+                    // console.log(titles);
+                    // res.setHeader('content-Type', 'application/json');
+                    // res.writeHead(200);
+                    // res.end(JSON.stringify(titles));
+                    break;
+                case '/getTitles':
+                    console.log(qs);
+                    const titles = await bdsutil.searchTitles('Madhu', 'file1.xml.json', 'title', qs.searchkey, qs.pagenum);
                     res.setHeader('content-Type', 'application/json');
                     res.writeHead(200);
                     res.end(JSON.stringify(titles));
                     break;
                 default:
-                    res.setHeader('content-Type', `${url.search(/.js/) !== -1 ? 'text/javascript' : 'image/png'}`);
+                    res.setHeader('content-Type', `${pathname.search(/.js/) !== -1 ? 'text/javascript' : 'image/png'}`);
                     res.writeHead(200);
-                    fs.readFile(path.join(__dirname, url), (err, data) => {
+                    fs.readFile(path.join(__dirname, pathname), (err, data) => {
                         res.end(data);
                     });
                     break;
